fb54432334c75d96ad5a916b4147b39ec773b3e4
Marc Nazarian <marc.nazarian@persistentsas.com>
2015-01-13 16:47:20 +0100

    Renamed folders and solution

---
+++ WcfBookServiceLibraryTest/GoogleApi/GoogleApiBookWrapperTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web.Script.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfBookServiceLibrary.Book;
+using WcfBookServiceLibrary.GoogleApi;
+
+namespace WcfBookServiceLibraryTest.GoogleApi
+{
+    [TestClass]
+    public class GoogleApiBookWrapperTest
+    {
+        private const string FixturesPath = ".GoogleApi.fixtures.";
+
+        [TestMethod]
+        public void DeserializeJsonResponseAndBuildBookInfo()
+        {
+            string jsonBookFixture = "google-api-impact-mapping.json";
+            CopyResource(jsonBookFixture);
+            String jsonBookContent = File.ReadAllText(jsonBookFixture);
+
+            JavaScriptSerializer jsSerializer = new JavaScriptSerializer();
+            GoogleApiBookWrapper googleApiBookWrapper = jsSerializer.Deserialize<GoogleApiBookWrapper>(jsonBookContent);
+            IBookInfo bookInfo = googleApiBookWrapper.GetBookInfo;
+
+            Assert.AreEqual("Impact Mapping", bookInfo.Title);
+            Assert.AreEqual("Making a Big Impact with Software Products and Projects", bookInfo.SubTitle);
+            Assert.AreEqual(1, bookInfo.Authors.Count());
+            Assert.AreEqual("Gojko Adzic", bookInfo.Authors.ElementAt(0));
+            StringAssert.Contains(bookInfo.Description, "A practical guide to impact mapping, a simple yet incredibly effective method for");
+            Assert.AreEqual("http://books.google.com/books/content?id=6tNoMwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api", bookInfo.Cover);
+        }
+
+        [TestMethod]
+        public void DeserializeJsonResponseAndReturnNullWhenNotFound()
+        {
+            string jsonBookFixture = "google-api-not-found.json";
+            CopyResource(jsonBookFixture);
+            String jsonBookNotFoundContent = File.ReadAllText(jsonBookFixture);
+
+            JavaScriptSerializer jsSerializer = new JavaScriptSerializer();
+            GoogleApiBookWrapper googleApiBookWrapper = jsSerializer.Deserialize<GoogleApiBookWrapper>(jsonBookNotFoundContent);
+            IBookInfo bookInfo = googleApiBookWrapper.GetBookInfo;
+
+            Assert.IsNull(bookInfo);
+        }
+
+        private static void CopyResource(string file)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            string resourceName = Assembly.GetExecutingAssembly().GetName().Name + FixturesPath + file;
+            using (Stream resource = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (resource == null)
+                {
+                    throw new ArgumentException("No such resource", "resourceName");
+                }
+                using (Stream output = File.OpenWrite(file))
+                {
+                    resource.CopyTo(output);
+                }
+            }
+        }
+    }
+}
---
+++ WcfBookServiceLibraryTest/Services/BookSearchServiceTest.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using System.Web.Script.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NMock;
+using WcfBookServiceLibrary;
+using WcfBookServiceLibrary.Book;
+using WcfBookServiceLibrary.Services;
+
+namespace WcfBookServiceLibraryTest.Services
+{
+    [TestClass]
+    public class BookSearchTest
+    {
+        private const string GoodIsbn = "0955683645";
+        private const string WrongIsbn = "666";
+
+        [TestMethod]
+        public void ReturnsJsonResponseWithBookInfoWhenIsbnIsGood()
+        {
+            string bookTitle = "Impact mapping";
+            string bookSubTitle = "Making a big impact with software products and projects";
+            string bookAuthor = "Gojko Adzik";
+            string bookDescription = "long description for Impact mapping book";
+            string bookCover = "http://books.google.com/books/content?id=6tNoMwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api";
+
+            IBookInfo impactMappingBookInfo = BookBuilder.GetInstance()
+                .WithTitle(bookTitle)
+                .WithSubTitle(bookSubTitle)
+                .WithAuthors(bookAuthor)
+                .WithDescription(bookDescription)
+                .WithCover(bookCover)
+                .Build();
+
+            MockFactory mockFactory = new MockFactory();
+            Mock<IsbnSearchInvoker> isbnSearchInvokerMocked = mockFactory.CreateMock<IsbnSearchInvoker>();
+            isbnSearchInvokerMocked.Expects.One.Method(invoker => invoker.GetBookInfo(null))
+                .With(GoodIsbn)
+                .WillReturn(impactMappingBookInfo);
+
+            BookSearchService bookSearchService = new BookSearchService(isbnSearchInvokerMocked.MockObject);
+
+            string jsonBookInfo = bookSearchService.GetBookInfo(GoodIsbn);
+
+            JavaScriptSerializer jsSerializer = new JavaScriptSerializer();
+            IBookInfo bookInfo = jsSerializer.Deserialize<BookInfo>(jsonBookInfo);
+
+            Assert.AreEqual(bookTitle, bookInfo.Title);
+            Assert.AreEqual(bookSubTitle, bookInfo.SubTitle);
+            Assert.AreEqual(1, bookInfo.Authors.Count());
+            Assert.AreEqual(bookAuthor, bookInfo.Authors.ElementAt(0));
+            Assert.AreEqual(bookDescription, bookInfo.Description);
+            Assert.AreEqual(bookCover, bookInfo.Cover);
+        }
+
+        [TestMethod]
+        public void ReturnsNullWhenIsbnIsNotFound()
+        {
+            IBookInfo bookInfo = null;
+
+            MockFactory mockFactory = new MockFactory();
+            Mock<IsbnSearchInvoker> isbnSearchInvokerMocked = mockFactory.CreateMock<IsbnSearchInvoker>();
+            isbnSearchInvokerMocked.Expects.One.Method(invoker => invoker.GetBookInfo(null))
+                .With(WrongIsbn)
+                .WillReturn(bookInfo);
+
+            BookSearchService bookSearchService = new BookSearchService(isbnSearchInvokerMocked.MockObject);
+
+            string jsonBookInfo = bookSearchService.GetBookInfo(WrongIsbn);
+
+            Assert.IsNull(jsonBookInfo);
+        }
+    }
+}
