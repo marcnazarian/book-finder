904093e605d40f4d19ac14ed1769b5bb04aef67d
Marc Nazarian <marc.nazarian@persistentsas.com>
2015-01-20 11:52:17 +0100

    Add abstract invoker to ease development of other web services

--- WcfBookServiceLibrary/GoogleApi/GoogleApiIsbnSearchInvoker.cs
+++ WcfBookServiceLibrary/GoogleApi/GoogleApiIsbnSearchInvoker.cs
@@ -1,37 +1,24 @@
-﻿using System.IO;
-using System.Net;
-using System.Web.Script.Serialization;
+﻿using System.Web.Script.Serialization;
 using WcfBookServiceLibrary.Book;
 
 namespace WcfBookServiceLibrary.GoogleApi
 {
-    public class GoogleApiIsbnSearchInvoker : IsbnSearchInvoker
+    public class GoogleApiIsbnSearchInvoker : AbstractIsbnSearchInvoker
     {
         private const string GoogleApiBookV1IsbnSearchBaseUrl = "https://www.googleapis.com/books/v1/volumes?q=isbn:{0}";
 
-        public IBookInfo GetBookInfo(string isbn)
+        protected override string BuildIsbnSearchRequestUri(string isbn)
         {
-
-            string googleApiBookIsbnSearchUrl = string.Format(GoogleApiBookV1IsbnSearchBaseUrl, isbn);
-            
-            HttpWebRequest request = WebRequest.Create(googleApiBookIsbnSearchUrl) as HttpWebRequest;
-
-            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-            {
-                if (response.StatusCode == HttpStatusCode.OK)
-                {
-                    StreamReader reader = new StreamReader(response.GetResponseStream());
-                    string jsonResponse = reader.ReadToEnd();
-
-                    JavaScriptSerializer jsSerializer = new JavaScriptSerializer();
-                    GoogleApiBookWrapper googleApiBookWrapper = jsSerializer.Deserialize<GoogleApiBookWrapper>(jsonResponse);
-                    
-                    return googleApiBookWrapper.GetBookInfo;
-                }
-
-                return null;
-            }
-            
+            return string.Format(GoogleApiBookV1IsbnSearchBaseUrl, isbn);
         }
+
+        protected override IBookInfo BuildBookInfoFromResponse(string jsonResponse)
+        {
+            JavaScriptSerializer jsSerializer = new JavaScriptSerializer();
+            GoogleApiBookWrapper googleApiBookWrapper = jsSerializer.Deserialize<GoogleApiBookWrapper>(jsonResponse);
+
+            return googleApiBookWrapper.GetBookInfo;
+        }
+
     }
 }
